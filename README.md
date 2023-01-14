ac6f7a6b3b3a4d7e35575f622b23ec2cf3ba8b70
jordip <plana86@gmail.com>
2023-01-13 19:37:12 -0800

    added arguments

--- README.md
+++ README.md
@@ -1,22 +1,39 @@
 # Prompt Generator API
 A simple prompt generator API for Stable Diffusion / Midjourney / Dall-e based in Python.
 
-Loosely based in the implementation of the [FredZhang7/distilgpt2-stable-diffusion-v2](https://huggingface.co/FredZhang7/distilgpt2-stable-diffusion-v2) by the Automatic1111 extension script by [imrayya](https://github.com/imrayya/stable-diffusion-webui-Prompt_Generator).
+Based on the implementation of the [FredZhang7/distilgpt2-stable-diffusion-v2](https://huggingface.co/FredZhang7/distilgpt2-stable-diffusion-v2) model.
 
 **Work in progress!** Contributions are welcomed :)
-## Installation
-1. Install required libraries.
+## Requirements
+The API is based in flask and uses transformers package to interact with the model.
 
-        pip install gradio transformers flask flask_restful
+To install required libraries run:
 
-2. Run main.py
+        pip install --upgrade transformers flask flask_restful
 
-        python3 main.py
+## Usage
+The API currently provides a POST endpoint to generate the prompt, configured to run at /generate
 
-3. Send the input prompt URL-encoded to http://yourserver.path/generate/
+Run main.py and send POST requests with the following arguments in JSON.
+
+**Required arguments**
+- prompt
+  - The beginning of the prompt.
+
+**Optional arguments**
+- temperature
+  - A higher temperature will produce more diverse results, but with a higher risk of less coherent text
+- top_k
+  - The number of tokens to sample from at each step
+- max_length
+  - The maximum number of tokens for the output of the model
+- repetition_penalty
+  - The penalty value for each repetition of a token
+- num_return_sequences
+  - The number of results to generate
 
 ## TODO
-1. Parametrize default values
+1. ~~Parametrize default arguments~~
 2. Add blacklist filtering
 3. Smart cropping
 4. Throttling
--- main.py
+++ main.py
@@ -1,53 +1,101 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse, abort
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
-import gradio as gr
 import logging
+import json
+import uuid
+
+# default config
+save_to_file = False
+default_args = {
+    'temperature': {
+        'default': 0.9,
+        'range': [0,1]
+    },
+    'top_k': {
+        'default': 8,
+        'range': [1, 200]
+    },
+    'max_length': {
+        'default': 80,
+        'range': [1, 200]
+    },
+    'repetition_penalty': {
+        'default': 1.2,
+        'range': [0, 10]
+    },
+    'num_return_sequences': {
+        'default': 5,
+        'range': [1, 5]
+    },
+}
 
 app = Flask("PromptGeneratorAPI")
 api = Api(app)
 
-# TODO parametrize the following
-# default config
-temperature = 0.9
-top_k = 8
-max_length = 80
-repetition_penalty = 1.2
-num_return = 5
-
+parser = reqparse.RequestParser()
+parser.add_argument('prompt', required=True)
+# optional arguments
+for arg, def_arg in default_args.items():
+    parser.add_argument(arg, type=float, required=False)
 class PromptGenerator(Resource):
-    def get(self, input_prompt):
+
+    # validate range, set value
+    def validateArgs(self, args):
+        for arg, def_arg in default_args.items():
+            if arg in args and args[arg]:
+                if def_arg['range'][0] < args[arg] > def_arg['range'][1]:
+                    abort(500, message=f"{arg} out of range. Min {def_arg['range'][0]}, Max {def_arg['range'][1]}")
+                globals()[arg] = args[arg]
+            else:
+                globals()[arg] = def_arg['default']
+
+    # post method
+    def post(self):
+        args = parser.parse_args()
+        self.validateArgs(args)
+
+        prompt = args['prompt']
+        request_uuid = uuid.uuid4()
         try:
+            # build model
             tokenizer = GPT2Tokenizer.from_pretrained('distilgpt2')
             tokenizer.add_special_tokens({'pad_token': '[PAD]'})
             model = GPT2LMHeadModel.from_pretrained('FredZhang7/distilgpt2-stable-diffusion-v2')
         except Exception as e:
             logging.error(f"Exception encountered while attempting to install tokenizer")
-            return [], 500
+            abort(500, message=f"There was an error processing your request")
         try:
-            logging.debug(f"Generate new prompt from: \"{input_prompt}\"")
-            input_ids = tokenizer(input_prompt, return_tensors='pt').input_ids
+            # generate prompt
+            logging.debug(f"Generate new prompt from: \"{prompt}\"")
+            input_ids = tokenizer(prompt, return_tensors='pt').input_ids
             output = model.generate(input_ids, do_sample=True, temperature=temperature,
                                     top_k=top_k, max_length=max_length,
-                                    num_return_sequences=num_return,
+                                    num_return_sequences=num_return_sequences,
                                     repetition_penalty=repetition_penalty,
                                     penalty_alpha=0.6, no_repeat_ngram_size=1,
                                     early_stopping=True)
-            logging.debug("Generation complete!")
             tempString = []
             for i in range(len(output)):
                 tempString.append(
                     tokenizer.decode(output[i], skip_special_tokens=True)
                 )
 
+            # save results to file
+            if save_to_file:
+                with open(f"{request_uuid}.json", 'w') as f:
+                    json.dump(tempString, f)
+
             return tempString
 
         except Exception as e:
             logging.error(
                 f"Exception encountered while attempting to generate prompt: {e}")
-            return [], 500
+            abort(500, message=f"There was an error processing your request")
 
-api.add_resource(PromptGenerator, '/generate/<input_prompt>')
+
+api.add_resource(PromptGenerator, '/generate')
 
 if __name__ == '__main__':
+    # app.debug = True
     app.run()
